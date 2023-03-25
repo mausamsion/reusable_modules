2af6ee7241495f9bd24de9ed8590becf050153c5
Mausam Jain <mausamsion@gmail.com>
2023-03-25 15:06:09 +0530

    Create crawl_selenium.py

---
+++ Random/crawl_selenium.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# start by defining the options 
+options = webdriver.ChromeOptions() 
+options.headless = True # it's more scalable to work in headless mode 
+
+# normally, selenium waits for all resources to download 
+# we don't need it as the page also populated with the running javascript code. 
+options.page_load_strategy = 'none' 
+
+# this returns the path web driver downloaded 
+chrome_path = ChromeDriverManager().install() 
+chrome_service = Service(chrome_path) 
+
+# pass the defined options and service objects to initialize the web driver 
+driver = Chrome(options=options, service=chrome_service) 
+driver.implicitly_wait(5)
+
+url = ""
+driver.get(url) 
+soup = BeautifulSoup(driver.page_source)
